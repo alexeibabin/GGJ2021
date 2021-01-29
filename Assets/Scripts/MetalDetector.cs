384aa15665a1e5620451a2c178484c3260cd1d83
bgmoonactive <benny@moonactive.com>
2021-01-29 16:28:08 +0200

    player meta detector graphics and fov beeps after a threshold

--- Assets/Scripts/MetalDetector.cs
+++ Assets/Scripts/MetalDetector.cs
@@ -14,19 +14,28 @@ public class MetalDetector : MonoBehaviour
     public AudioClip beep;
 
     public float beepHz = 0;
-    
-    public float msToNextBeep;
 
-    public float timeSinceLastBeep = 0f;
+    public float fovDetectionActivationDistance = 0;
+    
+    [SerializeField]
+    private float msToNextBeep;
+    [SerializeField]
+    private float timeSinceLastBeep = 0f;
+
+    private Camera mainCamera = null;
+
+    public bool lookingAtObject = false;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
     }
 
     void CalculateBeepHz()
     {
         var myPosition = transform.position;
         var closestDistance = sensor.SensorRange;
+        GameObject closestObject = null;
         sensor.DetectedObjects.Sort((a, b) =>
             Mathf.RoundToInt(Vector3.Distance(a.transform.position, myPosition) - Vector3.Distance(b.transform.position, myPosition)));
 
@@ -36,16 +45,37 @@ public class MetalDetector : MonoBehaviour
             if (dist < closestDistance)
             {
                 closestDistance = dist;
+                closestObject = obj;
             }
         }
         
-        var value = 0f;
+        //distance to object form 0 to 1. when 1 is closest
+        var normalizedDistance = 0f;
+
         if (sensor.DetectedObjects.Count > 0)
         {
-            value = 1 - (closestDistance / sensor.SensorRange);
+
+            //if were closer to an object that fovDetectionActivationDistance
+            //we calculate if the player is looking towards the object, if he is, we increase the beeps frequency
+            //if not, we keep it at the frequency as though hes at the threshold.
+            if (closestDistance < fovDetectionActivationDistance)
+            {
+                var mainCameraTransform = Camera.main.transform;
+                var dot = Vector3.Dot(mainCameraTransform.forward,
+                    (closestObject.transform.position - mainCameraTransform.position)
+                    .normalized); // == 1 when player looks directly at it
+                var closestNormalized = (1 -(closestDistance / sensor.SensorRange));
+                var thresholdNormalized = (1 -(fovDetectionActivationDistance / sensor.SensorRange));
+                lookingAtObject = dot > 0;
+                normalizedDistance = Mathf.Lerp(thresholdNormalized, closestNormalized, Mathf.Clamp01(dot));
+            }
+            else
+            {
+                normalizedDistance = 1 - (closestDistance / sensor.SensorRange);
+            }
         }
 
-        beepHz = value != 0 ? Mathf.Lerp(minBeepPerSec, maxBeepPerSec, value) : 0;
+        beepHz = normalizedDistance != 0 ? Mathf.Lerp(minBeepPerSec, maxBeepPerSec, normalizedDistance) : 0;
         msToNextBeep = 1000 / beepHz;
     }
 
